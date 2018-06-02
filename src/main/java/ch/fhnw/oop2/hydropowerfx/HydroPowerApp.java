3b64dba0daf4aeb4d37b77151235ecfd4b78387e
Markus Winter <markus.winter@students.fhnw.ch>
2018-06-03 00:57:21 +0200

    prevent loose unsaved changes

--- src/main/java/ch/fhnw/oop2/hydropowerfx/HydroPowerApp.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/HydroPowerApp.java
@@ -1,12 +1,19 @@
 package ch.fhnw.oop2.hydropowerfx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import ch.fhnw.oop2.hydropowerfx.presentationmodel.RootPM;
 import ch.fhnw.oop2.hydropowerfx.view.RootPanel;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
 
 public class HydroPowerApp extends Application {
 
@@ -21,6 +28,31 @@ public class HydroPowerApp extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.setMinWidth(1024);
 		primaryStage.setMinHeight(768);
+
+		Platform.setImplicitExit(false);
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		   public void handle(WindowEvent we) {Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+			if(rootPM.getButtonNavControlSaveActive()) {
+				alert.setTitle("Close Confirmation Dialog");
+				alert.setHeaderText("Sie haben noch ungespeicherte Änderungen!");
+
+				ButtonType buttonDiscard = new ButtonType("Änderungen verwerfen");
+				ButtonType buttonSave = new ButtonType("Speichern und verlassen");
+
+				alert.getButtonTypes().setAll(buttonDiscard, buttonSave);
+
+				Optional<ButtonType> result = alert.showAndWait();
+				if (result.get() == buttonDiscard) {
+					Platform.exit();
+				}
+				if (result.get() == buttonSave) {
+					rootPM.savePowerStationList();
+					Platform.exit();
+				}
+			} else Platform.exit();
+		   }
+	   });
 
 		primaryStage.show();
 	}
